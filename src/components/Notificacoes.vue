c6cd509f744d424d27ecfdabf75243f5e4dc4dd5
Vinicios Neves <vinicios.neves@farfetch.com>
2021-10-19 10:10:21 -0300

    fim do video 4.2

--- src/components/Notificacoes.vue
+++ src/components/Notificacoes.vue
@@ -1,40 +1,50 @@
 <template>
   <div class="notificacoes">
-    <article class="message is-success">
-      <div class="message-header">Atenção!</div>
+    <article
+      class="message"
+      :class="contexto[notificacao.tipo]"
+      v-for="notificacao in notificacoes"
+      :key="notificacao.id"
+    >
+      <div class="message-header">{{ notificacao.titulo }}</div>
       <div class="message-body">
-        Aqui vai um texto de notificação bem bacana.
-      </div>
-    </article>
-    <article class="message is-warning">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">
-        Aqui vai um texto de notificação bem bacana.
-      </div>
-    </article>
-    <article class="message is-danger">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">
-        Aqui vai um texto de notificação bem bacana.
+        {{ notificacao.texto }}
       </div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
+import { TipoNotificacao } from "@/interfaces/INotificacao";
 
 export default defineComponent({
   name: "Notificacoes",
+  data () {
+    return {
+      contexto: {
+        [TipoNotificacao.SUCESSO]: 'is-success',
+        [TipoNotificacao.ATENCAO]: 'is-warning',
+        [TipoNotificacao.FALHA]: 'is-danger',
+      }
+    }
+  },
+  setup() {
+    const store = useStore();
+    return {
+      notificacoes: computed(() => store.state.notificacoes),
+    };
+  },
 });
 </script>
 
 <style scoped>
 .notificacoes {
-    position: absolute;
-    right: 0;
-    width: 300px;
-    padding: 8px;
-    z-index: 105;
+  position: absolute;
+  right: 0;
+  width: 300px;
+  padding: 8px;
+  z-index: 105;
 }
 </style>
