f65a6e4f4d9888d2d8b260327e0a4876c265028b
Vinicios Neves <vinicios.neves@farfetch.com>
2021-10-19 09:04:02 -0300

    fim da aula 3.4

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -21,6 +21,8 @@
 import { useStore } from "@/store";
 import { defineComponent } from "vue";
 
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes'
+
 export default defineComponent({
   name: "Formulario",
   props: {
@@ -42,12 +44,12 @@ export default defineComponent({
   methods: {
     salvar() {
       if (this.id) {
-        this.store.commit('ALTERA_PROJETO', {
+        this.store.commit(ALTERA_PROJETO, {
           id: this.id,
           nome: this.nomeDoProjeto
         })
       } else {
-        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
       this.nomeDoProjeto = "";
       this.$router.push('/projetos')
