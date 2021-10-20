f1ad449176513242aaa09c865ec53d842a9ca0ee
Vinicios Neves <vinicios.neves@farfetch.com>
2021-10-20 09:08:04 -0300

    fim da aula 5.1

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -21,8 +21,10 @@
 import { useStore } from "@/store";
 import { defineComponent } from "vue";
 
-import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes'
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes'
 import { TipoNotificacao } from "@/interfaces/INotificacao";
+
+import { notificacaoMixin } from '@/mixins/notificar'
 
 export default defineComponent({
   name: "Formulario",
@@ -31,6 +33,7 @@ export default defineComponent({
       type: String
     }
   },
+  mixins: [notificacaoMixin],
   mounted () {
     if(this.id) {
       const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
@@ -52,14 +55,11 @@ export default defineComponent({
       } else {
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
+
       this.nomeDoProjeto = "";
-      this.store.commit(NOTIFICAR, {
-        titulo: 'Novo projeto foi salvo',
-        texto: 'Prontinho ;) seu projeto já está disponível.',
-        tipo: TipoNotificacao.SUCESSO
-      })
+      this.notificar(TipoNotificacao.ATENCAO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
       this.$router.push('/projetos')
-    },
+    }
   },
   setup () {
     const store = useStore()
